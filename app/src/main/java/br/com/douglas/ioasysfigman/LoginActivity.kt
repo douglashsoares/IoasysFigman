963a9f3e0fb2a2265d81b12eabbd799406fea8a7
Douglas <douglas.159@hotmail.com>
2022-01-23 20:37:31 -0300

    View Login Screen

---
+++ app/src/main/java/br/com/douglas/ioasysfigman/LoginActivity.kt
@@ -0,0 +1,23 @@
+package br.com.douglas.ioasysfigman
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.widget.TextViewCompat
+import com.google.android.material.button.MaterialButton
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val txtError = findViewById<AppCompatTextView>(R.id.txtError)
+
+        val button = findViewById<MaterialButton>(R.id.btnEnter)
+
+        button.setOnClickListener{
+            txtError.visibility = View.VISIBLE
+        }
+    }
+}
